9197d71ec643e698cde59ed4e1208283489adf62
putviindrisa <putviindri92@gmail.com>
2025-04-08 19:14:27 +0700

    Add files via upload

---
+++ soal4/soal4/src/App.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class App {
+    public static boolean hasDuplicates(int[] numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+        for (int num : numbers) {
+            
+            if (!uniqueNumbers.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {20, 1, 3, 2, 4, 6, 8, 5, 7, 9, 11, 13, 15, 10, 12, 14, 16, 18, 20, 17, 19};
+        System.out.println(hasDuplicates(data));
+    }
+}
